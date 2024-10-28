18738f47f8c1c1eae57ee90635449ae8b004191c
MRINMOY MONDAL <mm2922894@gmail.com>
2024-10-28 18:36:35 +0530

    completed lecture 2 hw

---
+++ lecture_02/calculator.hw.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int mode;
+  double valueOne, valueTwo, result;
+
+  cout << "enter your first number: ";
+  cin >> valueOne;
+  cout << "enter your second number: ";
+  cin >> valueTwo;
+
+  cout << "1-Sum, 2-Minus, 3-Multiply, 4-Devide, 5-Module what to perform: ";
+  cout << "choose what to perform: ";
+  cin >> mode;
+
+  if (mode == 1) {
+    result = valueOne + valueTwo;
+  } else if (mode == 2) {
+    result = valueOne - valueTwo;
+  } else if (mode == 3) {
+    result = valueOne * valueTwo;
+  } else if (mode == 4) {
+    result = valueOne / valueTwo;
+  } else if (mode == 5) {
+    /* result = valueOne % valueTwo; */
+  }
+
+  cout << "the result is : " << result << endl;
+
+  return 0;
+}
---
+++ lecture_02/code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "namaste" << endl;
+  cout << "bharat\n";
+  cout << "not\nscripter\n";
+
+  return 0;
+}
